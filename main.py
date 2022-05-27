b08fb98b80b7f434fd70741920382dd4c79bc370
seyi-pierce <100787067+seyi-pierce@users.noreply.github.com>
2022-05-26 19:47:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ chrome_driver_path = "C:/Users/19402/Applications/chromedriver"
 twitter_bot = InternetSpeedTwitterBot(chrome_driver_path)
 twitter_bot.get_internet_speed()
 time.sleep(10)
-twitter_bot.tweet_at_provider(username=TWITTER_EMAIL, password=TWITTER_PASSWORD, down=PROMISED_DOWN, up=PROMISED_UP)
+if twitter_bot.up < PROMISED_UP or twitter_bot.down < PROMISED_DOWN:
+  twitter_bot.tweet_at_provider(username=TWITTER_EMAIL, password=TWITTER_PASSWORD, down=PROMISED_DOWN, up=PROMISED_UP)
 
 
 
