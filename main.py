44bdda79fa490c26ecfd4785616cc60450adcb3f
seyi-pierce <100787067+seyi-pierce@users.noreply.github.com>
2022-05-26 19:45:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from internet_speed import InternetSpeedTwitterBot
 
 PROMISED_DOWN = 150
 PROMISED_UP = 10
-TWITTER_EMAIL = "adieleseyi@gmail.com"
-TWITTER_PASSWORD = "Donsexy2022$"
+TWITTER_EMAIL = input("Give twitter username")
+TWITTER_PASSWORD = input("Give twitter password")
 chrome_driver_path = "C:/Users/19402/Applications/chromedriver"
 
 twitter_bot = InternetSpeedTwitterBot(chrome_driver_path)
-# twitter_bot.get_internet_speed()
+twitter_bot.get_internet_speed()
 time.sleep(10)
 twitter_bot.tweet_at_provider(username=TWITTER_EMAIL, password=TWITTER_PASSWORD, down=PROMISED_DOWN, up=PROMISED_UP)
 
